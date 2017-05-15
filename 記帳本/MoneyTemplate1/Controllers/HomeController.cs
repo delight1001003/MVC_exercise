36e2a4de1c220652c46b9bb98ced7a0b91e7600c
沈怡安 <delight1001003@gmail.com>
2017-05-15 12:56:51 +0800

    contact with SkillTree Database(1000 data) - MoneyTemplate1

--- 記帳本/MoneyTemplate1/Controllers/HomeController.cs
+++ 記帳本/MoneyTemplate1/Controllers/HomeController.cs
@@ -12,37 +12,63 @@ namespace MoneyTemplate1.Controllers
 
         public ActionResult Index()
         {
+            var db = new SkillTreeModels();
             var list = new List<MyClass>();
-            MyClass model;
-            var random = new Random();
-
-            for (int i = 0; i<200; i++)
+            
+            foreach(var data in db.AccountBook)
             {
-                model = new MyClass();
+                var myClassData = new MyClass();
 
-                //用亂數給 Type 內容
-                int tempType = random.Next(2);
-                if ( tempType == 0 )
+                if(data.Categoryyy == 0)
                 {
-                    model.Type = "收入";
+                    myClassData.Type = "收入";
                 }
                 else
                 {
-                    model.Type = "支出";
-                }              
+                    myClassData.Type = "支出";
+                }
 
-                //用亂數給 Time 內容 (時間的range為5年 → 365天 * 5 )
-                model.Time = DateTime.Now.AddDays(random.Next(1826));
 
-                //用亂數給 Money 內容
-                model.Money = random.Next(50001);
-               
+                myClassData.Time = data.Dateee;
 
-                list.Add(model);
+                myClassData.Money = data.Amounttt;
+
+                list.Add(myClassData);
             }
-            
 
             return View(list);
+
+            //var list = new List<MyClass>();
+            //MyClass model;
+            //var random = new Random();
+
+            //for (int i = 0; i<200; i++)
+            //{
+            //    model = new MyClass();
+
+            //    //用亂數給 Type 內容
+            //    int tempType = random.Next(2);
+            //    if ( tempType == 0 )
+            //    {
+            //        model.Type = "收入";
+            //    }
+            //    else
+            //    {
+            //        model.Type = "支出";
+            //    }              
+
+            //    //用亂數給 Time 內容 (時間的range為5年 → 365天 * 5 )
+            //    model.Time = DateTime.Now.AddDays(random.Next(1826));
+
+            //    //用亂數給 Money 內容
+            //    model.Money = random.Next(50001);
+
+
+            //    list.Add(model);
+            //}
+
+
+            //return View(list);
         }
 
         public ActionResult About()
