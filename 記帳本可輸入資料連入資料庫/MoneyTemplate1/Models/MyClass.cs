af37d0e8868f930d6f02ef7ea056b69d3d2055ec
沈怡安 <delight1001003@gmail.com>
2017-05-17 12:07:22 +0800

    Update MyClass - add  property Remark

--- 記帳本可輸入資料連入資料庫/MoneyTemplate1/Models/MyClass.cs
+++ 記帳本可輸入資料連入資料庫/MoneyTemplate1/Models/MyClass.cs
@@ -10,5 +10,6 @@ namespace MoneyTemplate1.Models
         public string Type { get; set; }
         public DateTime Time { get; set; }
         public int Money { get; set; }
+        public string Remark { get; set; }
     }
 }
